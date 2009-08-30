4963ee63198e8ddc17cf0865b45073c1dc89c229
andrey <andrey@localhost>
2009-08-30 16:56:52 +0300

    [+] Extend message class with wall parameters

--- userapi/Message.py
+++ userapi/Message.py
@@ -1,13 +1,25 @@
 import Parser
 
 class Message:
-    def __init__(self, id, time, text, mfrom, mto, isReaded):
+    def __init__(self, id, time, text, mfrom, mto, isReaded,
+                # Parameters defined at wall's messages retrieval
+                message_type = 0, message_name = None,
+                thumb_url = None, original_url = None,
+                owner_id = None, item_id = None,
+                item_size = None):
         self.id        = id
         self.time      = time
         self.text      = text
         self.mfrom     = mfrom
         self.mto       = mto
         self.isReaded  = isReaded
+        self.message_type = message_type
+        self.message_name = message_name
+        self.thumb_url = thumb_url
+        self.original_url = original_url
+        self.owner_id  = owner_id
+        self.item_id   = item_id
+        self.item_size = item_size
 
 
 class Messages:
