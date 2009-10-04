1965c8019a2ffcc309cf88f884f87aba5772edb7
avatar <avatar@localhost>
2009-10-04 08:49:23 +0300

    [*] Move test_05 -> test_profile [+] test_05     - test own profile [+] test_05_1   - test real profile

--- uapi.py
+++ uapi.py
@@ -6,29 +6,8 @@ USER = "test.vkontakte@mail.ru"
 PASS = "yi8zXpHIs"
 DID="41657126"
 
-def test_01():
-    print "Own ID: " + str(test.get_own_id())
-
-def test_02():
-    Friends = test.v_friends(UserAPITypes.OnlineFirends, test.get_own_id(), 0, 100)
-    for friend in Friends:
-        print friend.name + "\t<" + str(friend.id) + ">"
-
-def test_03():
-    print test.v_friends(None, DID, 1, 100)
-    print test.v_friends(UserAPITypes.OnlineFirends, DID, 1, 100)
-
-def test_04():
-    inbox = test.v_messages(UserAPITypes.Inbox, None, 0, 100)
-    outbox = test.v_messages(UserAPITypes.Outbox, None, 0, 100)
-    conversations = test.build_conversations(inbox.messages, outbox.messages)
-    for conversation in conversations:
-        print "conversation: " + conversation.mwith.name
-        for message in conversation.messages:
-            print  "\t<" + message.text + ">"
-
-def test_05():
-    profile = test.v_profile()
+def test_profile(id = None):
+    profile = test.v_profile(id)
     print "Profile of user " + profile.name + " <" + str(profile.id) + "> "
     print "Mother: " + str(profile.mother)
     print "Status: "   + str(profile.status)
@@ -69,12 +48,42 @@ def test_05():
     print "Education statements not implemented fully yet:"
     print str(profile.education)
 
+
+def test_01():
+    print "Own ID: " + str(test.get_own_id())
+
+def test_02():
+    Friends = test.v_friends(UserAPITypes.OnlineFirends, test.get_own_id(), 0, 100)
+    for friend in Friends:
+        print friend.name + "\t<" + str(friend.id) + ">"
+
+def test_03():
+    print test.v_friends(None, DID, 1, 100)
+    print test.v_friends(UserAPITypes.OnlineFirends, DID, 1, 100)
+
+def test_04():
+    inbox = test.v_messages(UserAPITypes.Inbox, None, 0, 100)
+    outbox = test.v_messages(UserAPITypes.Outbox, None, 0, 100)
+    conversations = test.build_conversations(inbox.messages, outbox.messages)
+    for conversation in conversations:
+        print "conversation: " + conversation.mwith.name
+        for message in conversation.messages:
+            print  "\t<" + message.text + ">"
+
+def test_05():
+    test_profile()
+
+def test_05_1():
+    test_profile(5368780)
+
 def test_06():
     wall = test.v_wall(test.get_own_id(), 0, 100)
     for message in wall.messages:
         print message.mfrom.name + "\t<" + message.text + ">" + "\t == " + (message.original_url or "--")
 
-tests = [test_01, test_05, test_06]
+
+
+tests = [test_01, test_05, test_05_1, test_06]
 
 try:
     session = Session()
